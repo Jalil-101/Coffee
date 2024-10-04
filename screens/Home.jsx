0a4590259bbec606b6464c6c3d0fca83e13d8217
Jalil-101 <abdulmahama30@gmail.com>
2024-10-04 18:11:39 +0000

    Coffe initial

---
+++ screens/Home.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="text-xl text-blue-500">Home</Text>
+    </View>
+  );
+}
+
+export default Home
